bf4e2b1e62e1ccd6ec3b073ed316d46c9ef3ed0f
web2ls <web2ls@yandex.ru>
2022-11-26 20:23:42 +0600

    initial commit

---
+++ src/Components/PlanetObject.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let width;
+	export let height;
+	export let planet;
+	console.log(planet);
+</script>
+
+<div
+	class="planet-object"
+	style="width: {planet.width}; height: {planet.height}"
+>
+	Moon
+</div>
---
+++ src/Layout/SpeedContainer.svelte
@@ -0,0 +1,93 @@
+<script>
+	import PlanetObject from '../Components/PlanetObject.svelte';
+
+	import planetObjects from '../assets/data/planetObjects.json';
+
+	console.log(planetObjects);
+	console.log(planetObjects['moon']);
+	let currentPlanet = planetObjects['moon'];
+
+	const onSelectPlanet = (name) => {
+		currentPlanet = planetObjects[name];
+	};
+
+	const onClickEarth = () => {
+		console.log('yep');
+	};
+</script>
+
+<div class="speed-container">
+	<div class="object-selector">
+		<select
+			on:change={(event) => onSelectPlanet(event.currentTarget.value)}
+		>
+			<option value="moon">Moon</option>
+			<option value="uran">Uran</option>
+			<option value="jupiter">Jupiter</option>
+		</select>
+	</div>
+	<div class="earth" on:click={onClickEarth} on:keypress={onClickEarth}>
+		<div class="light" />
+	</div>
+	<PlanetObject
+		width={currentPlanet.width}
+		height={currentPlanet.height}
+		planet={currentPlanet}
+	/>
+	<div class="moon" />
+</div>
+
+<style>
+	.speed-container {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-around;
+		align-items: center;
+		width: 100%;
+		height: 100vh;
+		position: relative;
+		background: url(https://images.webfrontier.ru/pexels-hristo-fidanov-1252890.jpg)
+			no-repeat center;
+		background-size: cover;
+	}
+
+	.earth {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 50px;
+		height: 50px;
+		background: url(https://images.webfrontier.ru/image-from-rawpixel-id-6293608-png.png)
+			no-repeat center;
+		background-size: cover;
+		border-radius: 50%;
+	}
+
+	.moon {
+		width: 50px;
+		height: 50px;
+		background: url(https://images.webfrontier.ru/image-from-rawpixel-id-3474691-png.png)
+			no-repeat center;
+		background-size: cover;
+		border-radius: 50%;
+	}
+
+	.light {
+		width: 10px;
+		height: 10px;
+		background: transparent;
+		background-size: cover;
+		border-radius: 50%;
+		transition: transform 1.28s linear;
+	}
+
+	.active-light {
+		background: #fff;
+	}
+
+	.object-selector {
+		position: absolute;
+		top: 5px;
+		left: 10px;
+	}
+</style>
